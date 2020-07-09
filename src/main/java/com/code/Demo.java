f88d2382008eb67d4a68ae41cb1fe0161353c847
baoquan.ju <baoquan.ju@zhaopin.com.cn>
2020-07-09 21:43:33 +0800

    first

---
+++ src/main/java/com/code/Demo.java
@@ -0,0 +1,14 @@
+package com.code;
+
+/**
+ * 类
+ *
+ * @author Ju Baoquan
+ * Created at  2020/7/9
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
